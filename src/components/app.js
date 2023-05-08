ed87c09ddb7eef5d6bc4152da71ab658fdfc690e
hakanyarman <104122864+hakanyarman@users.noreply.github.com>
2023-05-08 15:07:16 +0300

    first commit

---
+++ src/components/app.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Header from "./header";
+import Footer from "./footer";
+import Note from "./note";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Footer />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/note.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1> Note  </h1>
+      <p>
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+        ea."
+      </p>
+    </div>
+  );
+}
+
+export default Note;
